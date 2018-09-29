2f034cd7eff730e20154ec9fdfa2a3025ed1432c
Clyde McQueen <clyde@mcqueen.net>
2018-09-29 12:24:10 -0700

    Estimate marker pose, publish tf, view in rviz

--- flock_base/src/detect_aruco.py
+++ flock_base/src/detect_aruco.py
@@ -4,16 +4,32 @@ import rospy
 from sensor_msgs.msg import Image
 import cv2
 from cv_bridge import CvBridge
+import tf
+import tf.transformations
+import numpy as np
 
 
 class DetectArUco(object):
 
+    # Fake data TODO provide data for Tello camera(s)
+    _camera_matrix = np.array([
+        [503.177416, 0.000000, 321.497483],
+        [0.000000, 501.403779, 248.637294],
+        [0.000000, 0.000000, 1.000000]
+    ])
+    _distortion = np.array([0.156934, -0.276625, 0.000091, 0.001410, 0.0000000])
+
+    # Markers are 18cm x 18cm
+    _marker_length = 0.18
+
+
     def __init__(self):
         # Initialize ROS
-        rospy.init_node('detect_aruco_node', anonymous=True)
+        rospy.init_node('detect_aruco_node', anonymous=False)
 
         # ROS publishers
         self._image_pub = rospy.Publisher('image_marked', Image, queue_size=10)
+        self._tf_broadcaster = tf.TransformBroadcaster()
 
         # ROS subscriptions
         rospy.Subscriber("image_raw", Image, self.image_callback)
@@ -38,15 +54,36 @@ class DetectArUco(object):
         # Detect markers
         corners, ids, _ = cv2.aruco.detectMarkers(gray_mat, self._aruco_dict, parameters=self._aruco_parameters)
 
-        # Publish list of corners
-        # TODO
+        # Compute pose
+        rvecs, tvecs, object_points = cv2.aruco.estimatePoseSingleMarkers(corners, self._marker_length, self._camera_matrix, self._distortion)
+
+        # Publish pose to marker0 as a TF message
+        if tvecs is not None and rvecs is not None:
+            # Rodrigues => 3x3 matrix
+            R3x3, _ = cv2.Rodrigues(rvecs[0][0])
+            # print(R3x3)
+
+            # transformations.py wants a 4x4 matrix -- probably a better way to do this
+            R4x4 = np.zeros((4, 4))
+            R4x4[:3, 0] = R3x3[:, 0]
+            R4x4[:3, 1] = R3x3[:, 1]
+            R4x4[:3, 2] = R3x3[:, 2]
+            R4x4[3,3] = 1
+            # print(R4x4)
+
+            # 4x4 matrix => quaternion
+            q = tf.transformations.quaternion_from_matrix(R4x4)
+            # print(q)
+
+            # Broadcast transform
+            self._tf_broadcaster.sendTransform(tvecs[0][0], q, rospy.Time.now(), 'camera_link_frame', 'marker_frame')
 
         # Draw border on the color image
         color_mat = cv2.aruco.drawDetectedMarkers(color_mat, corners)
 
-        # Debugging display TODO remove this
-        cv2.imshow('Markers', color_mat)
-        cv2.waitKey(1)
+        # Debugging display
+        # cv2.imshow('Markers', color_mat)
+        # cv2.waitKey(1)
 
         # Publish marked up image
         self._image_pub.publish(self._cv_bridge.cv2_to_imgmsg(color_mat, 'bgr8'))
