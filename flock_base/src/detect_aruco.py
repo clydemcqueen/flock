6bbc829b3ec1b560aacfd582b9eef62c3ea8fd23
Clyde McQueen <clyde@mcqueen.net>
2018-10-03 15:32:27 -0700

    Display markers in rviz

--- flock_base/src/detect_aruco.py
+++ flock_base/src/detect_aruco.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 
 import rospy
+from geometry_msgs.msg import Pose, Point, Quaternion, Vector3
 from sensor_msgs.msg import Image
-from visualization_msgs.msg import Marker
-from visualization_msgs.msg import MarkerArray
+from std_msgs.msg import ColorRGBA
+from visualization_msgs.msg import Marker, MarkerArray
 import cv2
 from cv_bridge import CvBridge
 import tf
-import tf.transformations
+import tf.transformations as tft
 import numpy as np
 
 # Transformation notation:
@@ -17,39 +18,61 @@ import numpy as np
 
 def rvec_and_tvec_to_matrix(rvec, tvec):
     """Rodrigues rotation and translation vector to 4x4 matrix"""
+    t_matrix = tft.translation_matrix(tvec)
     R, _ = cv2.Rodrigues(rvec)
-    T = np.identity(4)
-    T[:3, :3] = R
-    T[:3, 3] = tvec
-    return T
+    r_matrix = tft.identity_matrix()
+    r_matrix[:3, :3] = R
+    return np.dot(t_matrix, r_matrix)
 
 
 def tf_to_matrix(ros_transform):
     """ROS transform to 4x4 matrix"""
     t, q = ros_transform
-    T = tf.transformations.quaternion_matrix(q)
-    T[:3, 3] = t
-    return T
+    t_matrix = tft.translation_matrix(t)
+    r_matrix = tft.quaternion_matrix(q)
+    return np.dot(t_matrix, r_matrix)
 
 
 def matrix_to_tf(T):
     """4x4 matrix to ROS transform"""
-    t = T[:3, 3]
-    q = tf.transformations.quaternion_from_matrix(T)
-    return (t, q)
+    t = tft.translation_from_matrix(T)
+    q = tft.quaternion_from_matrix(T)
+    return t, q
+
+
+def pose_to_matrix(p):
+    """geometry_msgs.msg.Pose to 4x4 matrix"""
+    t_matrix = tft.translation_matrix([p.position.x, p.position.y, p.position.z])
+    r_matrix = tft.quaternion_matrix([p.orientation.x, p.orientation.y, p.orientation.z, p.orientation.w])
+    return np.dot(t_matrix, r_matrix)
+
+
+def matrix_to_pose(matrix):
+    """4x4 matrix to geometry_msgs.msg.Pose"""
+    t, q = matrix_to_tf(matrix)
+    return Pose(Point(*t), Quaternion(*q))
 
 
 class DetectArUco(object):
 
     # Clyde's Tello calibration data
-    _camera_matrix = np.array([[921.170702, 0.000000, 459.904354], [0.000000, 919.018377, 351.238301], [0.000000, 0.000000, 1.000000]])
+    _camera_matrix = np.array(
+        [[921.170702, 0.000000, 459.904354],
+         [0.000000, 919.018377, 351.238301],
+         [0.000000, 0.000000, 1.000000]])
     _distortion = np.array([-0.033458, 0.105152, 0.001256, -0.006647, 0.000000])
 
     # Markers are 18cm x 18cm
     _marker_length = 0.18
 
-    # ID of the first marker we saw
-    _first_marker = -1
+    # ID of the first marker we see
+    _first_marker_id = -1
+
+    # Pose of the first marker is fixed
+    _first_marker_pose = Pose(Point(0, 0, 1), Quaternion(0.5, -0.5, -0.5, 0.5))
+
+    # Transform odom => coordinate frame of the first marker
+    _Tom = tft.inverse_matrix(pose_to_matrix(_first_marker_pose))
 
     def __init__(self):
         # ArUco data -- we're using 6x6 ArUco images
@@ -70,13 +93,9 @@ class DetectArUco(object):
         self._tf_listener = tf.TransformListener()
         self._tf_broadcaster = tf.TransformBroadcaster()
 
-        # Get a few key static transforms
-        time = rospy.Time()
-        wait = rospy.Duration(4)
-        self._tf_listener.waitForTransform("marker_frame", "odom", time, wait)  # target, source
-        self._Tom = tf_to_matrix(self._tf_listener.lookupTransform("marker_frame", "odom", time))  # target, source
-        self._tf_listener.waitForTransform("base_link", "camera_frame", time, wait)
-        self._Tcb = tf_to_matrix(self._tf_listener.lookupTransform("base_link", "camera_frame", time))
+        # Get base_link => camera_frame transform
+        self._tf_listener.waitForTransform("base_link", "camera_frame", rospy.Time(), rospy.Duration(4))
+        self._Tcb = tf_to_matrix(self._tf_listener.lookupTransform("base_link", "camera_frame", rospy.Time()))
 
         # Now that we're initialized, set up subscriptions and spin
         rospy.Subscriber("image_raw", Image, self.image_callback)
@@ -96,55 +115,66 @@ class DetectArUco(object):
         if ids is None:
             return
 
-        # Grab the first marker we see
-        if self._first_marker < 0:
-            self._first_marker = ids[0][0]
-            rospy.loginfo('First marker has id %d' % self._first_marker)
-
         # Draw borders on the color image
         # color_mat = cv2.aruco.drawDetectedMarkers(color_mat, corners)
 
         # Publish the marked up image
         # self._image_pub.publish(self._cv_bridge.cv2_to_imgmsg(color_mat, 'bgr8'))
 
+        if self._first_marker_id < 0:
+            # Grab the first marker we see
+            self._first_marker_id = ids[0][0]
+            rospy.loginfo('First marker has id %d' % self._first_marker_id)
+        else:
+            # Stop if the first marker wasn't detected
+            found_first_marker = False
+            for index in range(len(ids)):
+                if ids[index][0] == self._first_marker_id:
+                    found_first_marker = True
+                    break
+            if not found_first_marker:
+                return
+
         # Compute transformations, each is marker_frame => camera_frame
         rvecs, tvecs, object_points = cv2.aruco.estimatePoseSingleMarkers(corners, self._marker_length, self._camera_matrix, self._distortion)
 
+        # Compute the pose of the drone
+        Tbo = tft.identity_matrix()
         for index in range(len(ids)):
-            if ids[index][0] == self._first_marker:
+            if ids[index][0] == self._first_marker_id:
                 # Tob = Tcb * Tmc * Tom
                 Tmc = rvec_and_tvec_to_matrix(rvecs[index][0], tvecs[index][0])
                 Tob = self._Tcb.dot(Tmc).dot(self._Tom)
 
-                # Broadcast the tf transform odom => base_link, where child=target, parent=source
+                # sendTransform expects child=target and parent=source
                 # We can't flip source and target because we want odom to be the parent of base_link
-                # Instead, we need to invert matrix Tob to get Tbo
-                Tbo = tf.transformations.inverse_matrix(Tob)
+                # Instead, invert Tob to get Tbo
+                Tbo = tft.inverse_matrix(Tob)
                 t, q = matrix_to_tf(Tbo)
                 self._tf_broadcaster.sendTransform(t, q, rospy.Time.now(), child='base_link', parent='odom')
                 break
 
+        # Compute the pose of the other markers, and publish a MarkerArray message for display in rviz
+        markerArray = MarkerArray()
         for index in range(len(ids)):
-            # TODO compute pose of all markers in odom frame, and add them
-            if ids[index][0] == self._first_marker:
-                marker = Marker()
-                marker.id = self._first_marker
-                marker.header.frame_id = 'odom'
-                marker.type = marker.CUBE
-                marker.scale.x = 0.01
-                marker.scale.y = 0.1
-                marker.scale.z = 0.1
-                marker.color.a = 1.0
-                marker.color.r = 1.0
-                marker.color.g = 1.0
-                marker.color.b = 0.0
-                marker.pose.orientation.w = 1.0
-                marker.pose.position.x = 0
-                marker.pose.position.y = 0
-                marker.pose.position.z = 0
-                markerArray = MarkerArray()
-                markerArray.markers.append(marker)
-                self._rviz_markers_pub.publish(markerArray)
+            marker = Marker()
+            marker.id = ids[index][0]
+            marker.header.frame_id = 'odom'
+            if ids[index][0] == self._first_marker_id:
+                # This one is easy
+                marker.pose = self._first_marker_pose
+            else:
+                # Compute the pose of this marker
+                # Tmo = Tbo * Tcb * Tmc
+                Tmc = rvec_and_tvec_to_matrix(rvecs[index][0], tvecs[index][0])
+                Tmo = Tbo.dot(self._Tcb).dot(Tmc)
+                marker.pose = matrix_to_pose(Tmo)
+            marker.type = marker.CUBE
+            marker.action = marker.ADD  # TODO DELETE markers that aren't visible
+            marker.scale = Vector3(0.1, 0.1, 0.01)
+            marker.color = ColorRGBA(1., 1., 0., 1.)
+            markerArray.markers.append(marker)
+        self._rviz_markers_pub.publish(markerArray)
 
 
 if __name__ == '__main__':
